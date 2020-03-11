98ef35fd1627b56b506f55de4f860c48f45ab9ef
HiredMussel <alexmusselwhite2@gmail.com>
2020-03-11 10:34:03 +0000

    expand on comments and add ids to individual cards

--- blackjack.php
+++ blackjack.php
@@ -12,39 +12,47 @@
  * @param String suitname - the name of the suit which will be generated
  * @return array - the 13 cards comprising the generated suit
  */
-function generateSuit($suitname) {
+function generateSuit(String $suitname) : Array {
     $suit = [];
+    // Generate the Ace
     $suit[1] = [
-        'Name' => 'Ace of ' . $suitname . '<br>',
+        'Name' => 'Ace of ' . $suitname,
         'Value' => 11,
     ];
     for ($i = 2; $i <= 10; $i++) {
         $suit[] = [
-            'Name' => $i . ' of ' . $suitname . '<br>',
+            'Name' => $i . ' of ' . $suitname,
             'Value' => $i,
         ];
     }
+    // Generate the Picture Cards
     $suit[11] = [
-        'Name' => 'Jack of ' . $suitname . '<br>',
+        'Name' => 'Jack of ' . $suitname,
         'Value' => 10,
     ];
     $suit[12] = [
-        'Name' => 'Queen of ' . $suitname . '<br>',
+        'Name' => 'Queen of ' . $suitname,
         'Value' => 10,
     ];
     $suit[13] = [
-        'Name' => 'King of ' . $suitname . '<br>',
+        'Name' => 'King of ' . $suitname,
         'Value' => 10,
     ];
     return $suit;
 }
 
 /**
- * Generates a standard deck of 52 cards without jokers
+ * Generates a standard deck of 52 cards without jokers. Do this by taking each suit and dealing them into the array.
+ * There are 13 cards in a suit. This function replicates the effect of simply stacking the suits on top of each other
+ * to create the deck, so there will be 13 cards between any two cards with the same value in neighbouring suits.
+ *
+ * The function also generates a unique id for each card corresponding to its initial position in the deck. This is so that
+ * we can check for specific cards later without having to check their value (e.g. distinguish between face cards and a 10
+ * for the purpose of detecting a Blackjack)
  *
  * @return array a standard deck of 52 cards without jokers
  */
-function generateDeck() {
+function generateDeck() : Array {
     $deck = null;
     $diamonds = generateSuit('Diamonds');
     $clubs = generateSuit('Clubs');
@@ -56,6 +64,9 @@ function generateDeck() {
         $deck[26+$i] = $hearts[$i];
         $deck[39+$i] = $spades[$i];
     }
+    for ($i = 1; $i <= 52; $i++) {
+        $deck[$i]['id'] = $i;
+    }
     return $deck;
 }
 
@@ -66,9 +77,12 @@ function generateDeck() {
  *                      the player's hand, so is passed by reference
  * @param $deck array the array representing the deck
  * @param $depth int draw the card this many cards from the top (dealing the same depth twice will result in duplicates)
+ *
+ * @return Boolean return 0 if the function executed successfully
  */
-function deal(&$playerHand, $deck, $depth) {
+function deal(Array &$playerHand, Array $deck, Int $depth) : Boolean {
     $playerHand[] = $deck[$depth];
+    return 0;
 }
 
 /**
@@ -94,6 +108,7 @@ function printScore($player) {
  */
 function playGame($player1, $player2) {
     $deck = generateDeck();
+    $depth = 0;
     shuffle($deck);
     deal($player1['Hand'], $deck, 0);
     deal($player2['Hand'], $deck, 1);
@@ -121,4 +136,7 @@ $player2=[
     'Name' => 'Player 2',
     'Score' => 0,
 ];
-playGame($player1, $player2);
+$deck = generateDeck();
+echo '<pre>';
+var_dump($deck);
+echo '</pre>';
